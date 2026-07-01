0d86f4728263a1a5c7d3c16192fcfc78dd7100f2
PushaRP <tanjuhanju@web.de>
2026-07-01 04:34:49 +0200

    Fix import order and update storageBucket URL

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 import { db, KATALOG_DOC, SETTINGS_DOC } from "./firebase-config.js";
-import { onSnapshot, getDoc, setDoc } from "firebase/firestore";
+import { onSnapshot, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDviEDCXO8UC4wAsN-v72G1bVyQu2rG1qk",
   authDomain: "pusharp-71b49.firebaseapp.com",
   projectId: "pusharp-71b49",
-  storageBucket: "pusharp-71b49.firebasestorage.app",
+  storageBucket: "pusharp-71b49.appspot.com",
   messagingSenderId: "879433057798",
   appId: "1:879433057798:web:6ca6b67c8c656fa9306587"
 };
@@ -13,6 +13,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
-// Firestore Docs
+// Firestore Dokumente
 export const KATALOG_DOC = doc(db, "katalog", "daten");
 export const SETTINGS_DOC = doc(db, "settings", "admin");
