8493f9054ee40108445e7f87131edfdd7546eb64
PushaRP <tanjuhanju@web.de>
2026-07-01 03:03:15 +0200

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDviEDCXO8UC4wAsN-v72G1bVyQu2rG1qk",
+  authDomain: "pusharp-71b49.firebaseapp.com",
+  projectId: "pusharp-71b49",
+  storageBucket: "pusharp-71b49.firebasestorage.app",
+  messagingSenderId: "879433057798",
+  appId: "1:879433057798:web:6ca6b67c8c656fa9306587",
+  measurementId: "G-9RY36C7YGW"
+};
